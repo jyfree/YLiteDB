9130d19b775f90fd1f6d0ee19098e1dae0b49af7
jy <123456>
2020-06-18 16:56:49 +0800

    修改api库

--- app/src/main/java/com/jy/simple/MyApplication.kt
+++ app/src/main/java/com/jy/simple/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.jy.simple
 
 import android.app.Application
+import com.jy.litedb.api.DBConfig
 import com.jy.litedb.api.DBManager
 import com.jy.simple.db.base.DBOpenHelper
 
@@ -19,7 +20,10 @@ class MyApplication : Application() {
             instance = this
         }
         //初始化数据库
-        DBManager.init(DBOpenHelper.getInstance(applicationContext))
+        DBManager.init(
+            DBOpenHelper.getInstance(applicationContext),
+            DBConfig.beginBuilder().build()
+        )
         //没有使用插件时，需要调用此方法
 //        DBManager.openDexFileLoaderService(this)
     }
